38547ccd149d3162cf76127f2febcbca86e2989c
Crowdbotics <team@crowdbotics.com>
2020-08-11 07:03:09 +0000

    Update models

--- agileai_19492/wsgi.py
+++ agileai_19492/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agileai_19492.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agileai_19492.settings")
 
 application = get_wsgi_application()
